30740a0267c5af0059411a1fc99b2927ee78d75b
Liliya Mykhailova <lm@codemotion.eu>
2022-01-31 12:41:43 +0200

    feat(icons): support custom icons urls, not only material-ui standard

--- src/components/ComponentIcon/ComponentIcon.tsx
+++ src/components/ComponentIcon/ComponentIcon.tsx
@@ -3,10 +3,12 @@ import Icon from "@material-ui/core/Icon";
 import SettingsIcon from "@material-ui/icons/Settings";
 import React from "react";
 import { useTreeSpec } from "../WithTreeSpec/WithTreeSpec";
+const isUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
 
 export const styles = {
   root: {
-    fontSize: (props: any) => (props.small ? "16px !important" : 24)
+    fontSize: (props: any) => (props.small ? "16px !important" : 24),
+    marginLeft: "6px"
   }
 };
 
@@ -25,7 +27,11 @@ const ComponentIcon: React.SFC<Props> = (props: Props) => {
   if (component && component.icon) {
     return (
       <Icon className={classes.root} {...other}>
-        {component.icon}
+        {component.icon.match(isUrl) ? (
+          <img style={{ width: "100%" }} src={component.icon} />
+        ) : (
+          component.icon
+        )}
       </Icon>
     );
   }
